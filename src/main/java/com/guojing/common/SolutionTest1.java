376b23d0f12598610072d8024e5ec37c1228ec52
guojing24 <guojing24@baidu.com>
2021-06-21 17:52:32 +0800

    test

---
+++ src/main/java/com/guojing/common/SolutionTest1.java
@@ -0,0 +1,14 @@
+package com.guojing.common;
+
+/**
+ * @author: guojing
+ * @date: 2021-06-21
+ * @time: 下午5:49
+ */
+public class SolutionTest1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
